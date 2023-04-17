47c76c59e6f302e85ee6623cfb8177b24c2117eb
Ahmed Izz Murtaja <56529633+ahmedmurtaja@users.noreply.github.com>
2023-04-17 15:18:21 +0200

    Create Server Relates #1 Closes #1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+const { log } = require('console');
 const app = require('./app');
 
-app.listen(app.get('port'),()=>{
-  console.log(`App is live on http://localhost:${app.get("port")}`);
+app.listen(app.get('port'), () => {
+  log(`App is live on http://localhost:${app.get('port')}`);
 });

fe0a9e8c197a672a753f4be4b9cf0c437ad8b211
Ahmed Izz Murtaja <56529633+ahmedmurtaja@users.noreply.github.com>
2023-04-18 17:11:53 +0200

    Create sign up Schema

--- src/utils/validation/signupSchema.js
+++ src/utils/validation/signupSchema.js
@@ -1,1 +1,9 @@
 const joi = require('joi');
+
+const signupSchema = joi.object({
+  username: joi.string().min(3).max(30).required(),
+  email: joi.string().email().required(),
+  password: joi.string().min(6).max(30).required(),
+});
+
+module.exports = signupSchema;

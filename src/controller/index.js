be66ecddad0204cd94a0eb27c22d2c68677ba9d3
Ahmed Izz Murtaja <56529633+ahmedmurtaja@users.noreply.github.com>
2023-04-20 14:29:30 +0200

    crete add post controller

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,7 +1,8 @@
 const getAllPosts = require('./getPostsController');
+const newPost = require('./newPostController');
 const signin = require('./signinController');
 const signup = require('./signupController');
 
 module.exports = {
-  signin, signup, getAllPosts,
+  signin, signup, getAllPosts, newPost,
 };

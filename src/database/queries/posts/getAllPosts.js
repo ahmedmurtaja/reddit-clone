7fedd36e25b763d0abc15c66c6009fc387a02f63
Ahmed Izz Murtaja <56529633+ahmedmurtaja@users.noreply.github.com>
2023-04-21 00:31:22 +0200

    split queries into files

--- src/database/queries/posts/getAllPosts.js
+++ src/database/queries/posts/getAllPosts.js
@@ -1,4 +1,4 @@
-const connection = require('../config/connection');
+const connection = require('../../config/connection');
 
 const getPosts = () => {
   const sql = {
--- src/database/queries/votes/voteQuery.js
+++ src/database/queries/votes/voteQuery.js
@@ -1,4 +1,4 @@
-const connection = require('../config/connection');
+const connection = require('../../config/connection');
 
 const voteQuery = (postId, userId, vote) => {
   const sql = {

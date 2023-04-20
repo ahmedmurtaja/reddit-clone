802e3852a6266ca8d8b32b7579c30dcd074d9d9a
Ahmed Izz Murtaja <56529633+ahmedmurtaja@users.noreply.github.com>
2023-04-20 15:38:59 +0200

    create new post route

--- src/controller/newPostController.js
+++ src/controller/newPostController.js
@@ -1,13 +1,16 @@
 const { insertPost } = require('../database/queries');
+const { postSchema } = require('../utils/validation');
 
 const newPost = (req, res, next) => {
   const { content } = req.body;
   const { id } = req.user;
-  insertPost(content, id)
+  postSchema.validateAsync({ content })
+    .then(() => insertPost(content, id))
     .then((data) => data.rows)
     .then((data) => res.json({
       success: true,
       data,
-    })).catch((err) => next(err));
+    }))
+    .catch((err) => next(err));
 };
 module.exports = newPost;

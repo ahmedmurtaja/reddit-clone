dbe1dd64a41d1a2642c54d4ed8848e550df48db4
Ahmed Izz Murtaja <56529633+ahmedmurtaja@users.noreply.github.com>
2023-04-23 13:05:21 +0200

    feat(back): handle logout route and controller Relates #20 Closes #20

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,7 +1,8 @@
 const userRouter = require('express').Router();
-const { signup, signin } = require('../controller');
+const { signup, signin, logout } = require('../controller');
 
 userRouter.post('/signup', signup);
 userRouter.post('/signin', signin);
+userRouter.get('/logout', logout);
 
 module.exports = userRouter;
